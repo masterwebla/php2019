bbd347364a9c40952c7ad9a9e723efae23dbc2c7
Mauricio Mac <info@masterweb.la>
2019-11-02 10:08:26 -0500

    Avances en listado de usuarios

---
+++ listar.php
@@ -0,0 +1,48 @@
+<?php
+	//CONECTARNOS CON LA BASE DE DATOS
+	require("conexion.php");
+
+	//HACER LA CONSULTA A LA TABLA USUARIOS
+	$sql_consultar = "SELECT * FROM usuarios";
+	$resultado = $conn->query($sql_consultar);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Listado de usuarios</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Listado de Usuarios</h1>
+		<table class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Nombre</th>
+					<th>Email</th>
+					<th>Fecha</th>
+					<th>Ciudad</th>
+					<th>Opciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php while($fila = $resultado->fetch(PDO::FETCH_ASSOC)){ ?>
+					<tr>
+						<td><?php echo $fila['nombre']; ?></td>
+						<td><?php echo $fila['email']; ?></td>
+						<td><?php echo $fila['fecha']; ?></td>
+						<td><?php echo $fila['ciudad']; ?></td>
+						<td></td>
+					</tr>
+				<?php } ?>
+			</tbody>
+		</table>
+	</div>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/boottrap.min.js"></script>
+</body>
+</html>
